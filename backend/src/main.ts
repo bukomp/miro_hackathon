2968c17c8a2e6656eaecceea4edb08cf1d1968f8
Riku Olkkonen <riku.olkkonen@aalto.fi>
2024-01-20 19:08:56 +0200

    added file upload api endpoint and text extraction

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,11 @@
 import axios from 'axios';
 import express from 'express';
 import { getEnvOrDefault, getEnvOrThrow } from './utils/config';
+import uploadFileRoutes from './routes/uploadFileRoutes';
 
 const app = express();
+
+app.use('/api', uploadFileRoutes);
 
 app.get('/', async (req, res) => {
   const data = await axios.get('https://miro.com/signup/');

839a352da1b0478029213cf73be3153a31e9489e
Riku Olkkonen <riku.olkkonen@aalto.fi>
2024-01-20 20:21:56 +0200

    added creating uploads directory if it odneskt s0idiffasd fpoasjdfölsandf

--- backend/src/routes/uploadFileRoutes.ts
+++ backend/src/routes/uploadFileRoutes.ts
@@ -1,9 +1,21 @@
-import express from 'express';
-import upload from '../middleware/uploadFileMiddleware';
-import textract from 'textract';
-import fs from 'fs/promises'
+import express from 'express'
+import upload from '../middleware/uploadFileMiddleware'
+import textract from 'textract'
+import fs from 'fs' // For synchronous operations
+import fsp from 'fs/promises' // For asynchronous operations
 
-const router = express.Router();
+const router = express.Router()
+
+// Create uploads directory if it doesn't exist
+const uploadsDir = './uploads'
+
+if (!fs.existsSync(uploadsDir)) {
+  fs.mkdirSync(uploadsDir, { recursive: true })
+}
+
+router.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 // Uploads file, extracts text, and returns combined text
 router.post('/uploadFile', upload.array('files', 10), async (req, res) => {
@@ -13,7 +25,7 @@ router.post('/uploadFile', upload.array('files', 10), async (req, res) => {
     }
 
     let combinedText = ''
-    
+
     for (const file of req.files) {
       const text = await new Promise((resolve, reject) => {
         textract.fromFileWithPath(file.path, (error, text) => {
@@ -25,9 +37,9 @@ router.post('/uploadFile', upload.array('files', 10), async (req, res) => {
       combinedText += text + '\n'
 
       // Delete the file after processing
-      await fs.unlink(file.path)
+      await fsp.unlink(file.path)
     }
-
+    console.log(combinedText)
     res.send({ combinedText: combinedText })
   } catch (error) {
     if (error instanceof Error) {
